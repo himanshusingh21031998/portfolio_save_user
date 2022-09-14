b595053130e91ecc7856ac98fbe9ecc7294cea41
himanshu <104294694+himanshusinghcoder@users.noreply.github.com>
2022-09-14 11:13:36 +0530

    get on iniaial route

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.post('/save_user_data', async (req, res)=> {
     }
 })
 
+
+app.get('/', (req,res) => {
+res.json({status: 'success'})
+}   
+)
+
 app.listen(5000, () => {
     console.log("app listening at port 5000");
 })

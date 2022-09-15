20c3377322e55dc03f64aa887070d9ee05b204f3
himanshu <104294694+himanshusinghcoder@users.noreply.github.com>
2022-09-15 14:49:33 +0530

    database change

--- mongo_config.js
+++ mongo_config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoConnect = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:admin123@cluster0.0i51ttw.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://admin:admin123@cluster0.0i51ttw.mongodb.net/users?retryWrites=true');
         console.log('Connection success');
       } catch (error) {
         console.log(error);
